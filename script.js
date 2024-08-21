1dd3dbae5943167812a4456e5c698767d9debe28
aaron <aaron.blanco@ucsp.edu.pe>
2024-08-20 21:17:00 -0500

    cambios_finales

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("barra");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
